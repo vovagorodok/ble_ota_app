07a55dc9119c60813e95b53a2e61da34fa690958
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-01-19 01:29:15 +0100

    More uuids

--- lib/src/ble/ble_uuids.dart
+++ lib/src/ble/ble_uuids.dart
@@ -1,3 +1,13 @@
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 final serviceUuid = Uuid.parse("15c155ca-36c5-11ed-adc0-9741d6a72f04");
+final characteristicUuidRx = Uuid.parse("15c1564c-36c5-11ed-adc1-a3d6cf5cc2a4");
+final characteristicUuidTx = Uuid.parse("15c156e2-36c5-11ed-adc2-7396d4fd413a");
+final characteristicUuidHwName =
+    Uuid.parse("15c1576e-36c5-11ed-adc3-8799895de51e");
+final characteristicUuidHwVer =
+    Uuid.parse("15c157fa-36c5-11ed-adc4-579c60267b47");
+final characteristicUuidSwName =
+    Uuid.parse("15c15886-36c5-11ed-adc5-1bc0d0a6069d");
+final characteristicUuidSwVer =
+    Uuid.parse("15c1591c-36c5-11ed-adc6-dbe9603dbf19");
