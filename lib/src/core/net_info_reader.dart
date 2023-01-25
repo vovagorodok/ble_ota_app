5728da2aa747f24445898feecaab8120759a5f5e
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-01-25 02:20:38 +0100

    Using http

--- lib/src/core/net_info_reader.dart
+++ lib/src/core/net_info_reader.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:ble_ota_app/src/core/hardware_info.dart';
 import 'package:ble_ota_app/src/core/softwate_info.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class NetInfoReader {
   final StreamController<SwInfoState> _infoStreamController =
@@ -12,8 +13,41 @@ class NetInfoReader {
   Stream<SwInfoState> get infoStream => _infoStreamController.stream;
   SwInfoState infoState = SwInfoState();
 
+  Future<void> _readSoftware(HardwareInfo hwInfo, String hardwarePath) async {
+    try {
+      final response = await http.get(Uri.parse(hardwarePath));
+      if (response.statusCode != 200) {
+        return;
+      }
+
+      final body = json.decode(response.body);
+      final fullList = body.map<SoftwareInfo>(SoftwareInfo.fromJson).toList();
+      final filteredByHwList = fullList.where((SoftwareInfo info) {
+        return info.hwName == hwInfo.hwName &&
+            (info.hwVer != null ? info.hwVer == hwInfo.hwVer : true) &&
+            (info.minHwVer != null ? info.minHwVer! <= hwInfo.hwVer : true) &&
+            (info.maxHwVer != null ? info.maxHwVer! >= hwInfo.hwVer : true);
+      }).toList();
+      final filteredBySwList = filteredByHwList.where((SoftwareInfo info) {
+        return info.name == hwInfo.swName;
+      }).toList();
+
+      infoState.swInfoList = filteredByHwList;
+      if (filteredBySwList.isEmpty) {
+        return;
+      }
+      final max = filteredBySwList.reduce((SoftwareInfo a, SoftwareInfo b) {
+        return a.ver >= b.ver ? a : b;
+      });
+      if (max.ver <= hwInfo.swVer) {
+        return;
+      }
+      infoState.newest = max;
+    } catch (_) {}
+  }
+
   void read(HardwareInfo hwInfo) {
-    infoState.ready = false;
+    infoState = SwInfoState();
     _infoStreamController.add(infoState);
 
     () async {
@@ -22,9 +56,7 @@ class NetInfoReader {
       final hardwarePath = body[hwInfo.hwName];
 
       if (hardwarePath != null) {
-        print("VOVA: hardwarePath: $hardwarePath");
-      } else {
-        print("VOVA: is NULL");
+        await _readSoftware(hwInfo, hardwarePath);
       }
 
       infoState.ready = true;
--- lib/src/core/version.dart
+++ lib/src/core/version.dart
@@ -19,6 +19,43 @@ class Version {
     return "$major.$minor.$patch";
   }
 
+  bool operator <=(Version other) {
+    if (major != other.major) {
+      return major < other.major;
+    }
+    if (minor != other.minor) {
+      return minor < other.minor;
+    }
+    if (patch != other.patch) {
+      return patch < other.patch;
+    }
+    return true;
+  }
+
+  bool operator >=(Version other) {
+    if (major != other.major) {
+      return major > other.major;
+    }
+    if (minor != other.minor) {
+      return minor > other.minor;
+    }
+    if (patch != other.patch) {
+      return patch > other.patch;
+    }
+    return true;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is Version &&
+        other.major == major &&
+        other.minor == minor &&
+        other.patch == patch;
+  }
+
+  @override
+  int get hashCode => major.hashCode ^ minor.hashCode ^ patch.hashCode;
+
   final int major;
   final int minor;
   final int patch;
