e6af2a43fa56285853451094f85f8878f88ef0e8
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-01-31 19:17:58 +0100

    Rename to BleOtaApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ble_ota_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BleOtaApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
