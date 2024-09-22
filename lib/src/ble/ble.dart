99160d2a146c8a286a3ada3e0aceac03bbdeaf53
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2024-09-22 17:32:09 +0200

    Changed imports order

--- lib/src/ble/ble.dart
+++ lib/src/ble/ble.dart
@@ -1,8 +1,8 @@
-import 'package:ble_ota_app/src/ble/ble_central.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'dart:io' show Platform;
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:bluetooth_low_energy/bluetooth_low_energy.dart';
+import 'package:ble_ota_app/src/ble/ble_central.dart';
 import 'package:ble_ota_app/src/ble/flutter_reactive_ble_central.dart';
 import 'package:ble_ota_app/src/ble/flutter_web_bluetooth_central.dart';
 import 'package:ble_ota_app/src/ble/universal_ble_central.dart';
