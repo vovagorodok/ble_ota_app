c37a94ee4dc45a41d37016b847cae525fbe4265b
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2026-02-01 23:49:55 +0100

    Fixed compilation without keystoreProperties

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,10 +41,12 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+            if (keystoreProperties.isNotEmpty()) {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
 
